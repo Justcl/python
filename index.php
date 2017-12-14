27d50d3b370e38b0abc589f4baddad962ed88b21
蔡黎 <10215510@qq.com>
2017-12-14 19:21:49 +0800

    yaf结合angularjs

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
 //phpinfo();die;
 define('APPLICATION_PATH', dirname(__FILE__));
-define("APP_HOST","http://www.yaf.com/");
+define("APP_HOST","http://www.yaf.com/public/");
 define("APP_NAME",'yaf_dev');
 $application = new Yaf_Application( APPLICATION_PATH . "/conf/application.ini");
-
 $application->bootstrap()->run();
 
