b14464e22aa7058e7a2640b5f9e9150c15d75669
蔡黎 <10215510@qq.com>
2019-07-17 11:11:17 +0800

    de

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-//phpinfo();die;
-define('APPLICATION_PATH', dirname(__FILE__));
-define("APP_HOST","http://www.yaf.com/public/");
-define("APP_NAME",'yaf_dev');
-$application = new Yaf_Application( APPLICATION_PATH . "/conf/application.ini");
-$application->bootstrap()->run();
-
